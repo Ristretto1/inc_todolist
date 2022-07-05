816fa98d5b8e359b4031baed3657e850929bfcc6
Antonov Kirill <antonovkir2@gmail.com>
2022-07-05 14:08:34 +0500

    fix API KEY

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '8f2534e2-22a4-4052-894e-a66c04807482'
+        'API-KEY': 'ac422c12-b2f2-4703-8702-4c62b2f04639'
     }
 })
 
