af623f7c5bc41e7ad5543da3fa40bd053ae540bf
Kirill Antonov <antonovkir2@gmail.com>
2025-04-24 12:58:48 +0300

    week_04

--- src/components/Button/Button.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from "react";
-import { IButton } from "./Button.types";
-
-export const Button: FC<IButton> = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
-};
